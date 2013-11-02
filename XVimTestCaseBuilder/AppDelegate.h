faaabec352577abb0146e97987e9b83960ae4475
Patrick <opensourcepatrick@gmail.com>
2013-11-02 14:27:28 -0700

    Initial Commit

---
+++ XVimTestCaseBuilder/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  XVimTestCaseBuilder
+//
+//  Created by Patrick on 11/2/13.
+//  Copyright (c) 2013 weaksauce. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ XVimTestCaseBuilder/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  XVimTestCaseBuilder
+//
+//  Created by Patrick on 11/2/13.
+//  Copyright (c) 2013 weaksauce. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
